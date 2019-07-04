afd91218f286094cf6e06656e9cdd29da9f05709
Jean-Philippe Sirois <jp@veksen.net>
2019-07-04 15:21:49 -0400

    feat: bootstrap project, working input/output

---
+++ src/App.tsx
@@ -0,0 +1,84 @@
+import React, { ChangeEvent, useState } from "react";
+import "./App.css";
+
+interface Library {
+  name: string;
+  version: string;
+}
+
+type PackageManager = "yarn" | "npm";
+
+function App() {
+  const [input, setInput] = useState("");
+  const [packageManager, setPackageManager] = useState<PackageManager>("npm");
+
+  function handleOnChange(e: ChangeEvent<HTMLTextAreaElement>) {
+    setInput(e.target.value);
+  }
+
+  function parse(str: string) {
+    const install = packageManager === "npm" ? "npm i" : "yarn";
+    const bumpLibrary = ({ name, version }: Library): string =>
+      `npx npm-check-updates -u ${name}; ${install}; git add -A; git commit -m "chore(deps): bump ${name} to ${version}"`;
+
+    if (!str) return "";
+
+    return str
+      .split(/\n/)
+      .map(line => line.trim())
+      .filter(Boolean)
+      .map(
+        (line): Library => {
+          const [name, , , version] = line.split(/ +/);
+          return {
+            name,
+            version: version.replace(/\^/, "")
+          };
+        }
+      )
+      .map(library => bumpLibrary(library))
+      .join("; ");
+  }
+
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="inline-radios">
+          <div className="inline-radio">
+            <input
+              id="yarn"
+              type="radio"
+              checked={packageManager === "yarn"}
+              onChange={() => setPackageManager("yarn")}
+            />
+            <label htmlFor="yarn">Yarn</label>
+          </div>
+          <div className="inline-radio">
+            <input
+              id="npm"
+              type="radio"
+              checked={packageManager === "npm"}
+              onChange={() => setPackageManager("npm")}
+            />
+            <label htmlFor="npm">npm</label>
+          </div>
+        </div>
+
+        <label htmlFor="input">
+          Input (copy/paste from <code>npx npm-check-updates</code>)
+        </label>
+        <textarea
+          id="input"
+          value={input}
+          onChange={handleOnChange}
+          rows={10}
+        />
+
+        <label htmlFor="output">Output (paste this in your console)</label>
+        <textarea id="output" value={parse(input)} rows={10} readOnly />
+      </div>
+    </div>
+  );
+}
+
+export default App;
